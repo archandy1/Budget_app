71e5ad1ee6a79ff9d41cc722973d0c6628457430
archandy1 <a.pleczkan@gmail.com>
2022-03-24 14:19:53 +0100

    First shetch of javascript

---
+++ main.js
@@ -0,0 +1,16 @@
+/* Budget app should disply 3 values:
+    - initial budget amount (value)
+    - expenses (description, value) there can be multiple expenses
+    - Balance (value after expenses)
+*/
+
+let initialBudget = () => {
+
+}
+
+function expenses(description, expAmount) {
+
+
+function balance() {
+    
+}
